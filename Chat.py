5ad239a526b317f810bdfc90b038f389c81b4781
unknown <sissokomahamaneyaya@gmail.com>
2024-07-23 01:31:50 -0400

    Resolved import issues and updated Chat.py

--- Chat.py
+++ Chat.py
@@ -1,10 +1,10 @@
+import os
 from flask import Flask, request, jsonify
 import base64
 import vertexai
 from vertexai.generative_models import GenerativeModel, Part
 import vertexai.preview.generative_models as generative_models
 import json
-
 
 # Define the texts for system instructions
 textsi_1 = """“You are an academic assistant designed to help students with their studies. Your primary role is to provide clear, accurate, and helpful information on a wide range of academic topics.”"""
@@ -13,7 +13,6 @@ textsi_3 = """“Use simple and clear language to explain complex concepts. Avoi
 textsi_4 = """“When asked for definitions or explanations, start with a brief overview and then provide more detailed information.”"""
 textsi_5 = """“Offer study tips and techniques, such as the Pomodoro method, to help students manage their time effectively.”"""
 
-
 # Define generation and safety settings
 generation_config = {
     "max_output_tokens": 8192,
@@ -21,14 +20,12 @@ generation_config = {
     "top_p": 0.95,
 }
 
-
 safety_settings = {
     generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
     generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
     generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
     generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
 }
-
 
 # Function to generate content using Vertex AI
 def multiturn_generate_content(chat, user_input):
@@ -39,7 +36,6 @@ def multiturn_generate_content(chat, user_input):
     )
     return response.to_dict()  # Convert the response to a dictionary
 
-
 # Initialize the Vertex AI model and start the chat
 vertexai.init(project="test-429206", location="us-central1")
 model = GenerativeModel(
@@ -48,9 +44,7 @@ model = GenerativeModel(
 )
 chat = model.start_chat(response_validation=False)  # Disable response validation
 
-
 app = Flask(__name__)
-
 
 @app.route('/api/chat', methods=['POST'])
 def chat_endpoint():
@@ -58,6 +52,6 @@ def chat_endpoint():
     ai_response = multiturn_generate_content(chat, user_message)  # Call your Python function
     return jsonify({'reply': ai_response})
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
